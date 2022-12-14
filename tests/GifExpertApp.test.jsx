b841f014bb2395e93ce30b2f49b7fbb3cc2e585e
carlos.fernandez <carlos.fernandez@tecnologiaypersonas.es>
2022-12-14 11:46:38 +0100

    Tarea Seccion 4

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { GifExpertApp } from '../src/GifExpertApp';
 
 //Tarea
 describe('Pruebas en <GifExpertApp />', () => { 
    
-    test('should first', () => { 
+    test('Debe haber dos <h3> despues de buscar un resultado nuevo', () => { 
 
         render( <GifExpertApp /> )
-        screen.debug();
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: { value: 'Goku' } } );
+        fireEvent.submit( form );
+
+        expect( screen.getByText('GifExpertApp') ).toBeTruthy();
+        expect( screen.getAllByRole( 'heading', { level: 3 } ).length ).toBe(2)
 
     });
 
